55401b153886ed29fe2d89f4449945b9967a9711
Samuel Nascimento <sfsnascimento@gmail.com>
2023-05-16 16:26:23 -0300

    adicionando niveis no app.use()

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,10 +1,12 @@
 const bodyParser = require('body-parser');
 const pessoas = require('./pessoasRoute');
 const turmas = require('./turmasRoute');
+const niveis = require('./niveisRoute');
 
 module.exports = app => {
   app.use(bodyParser.json(),
     pessoas,
-    turmas
+    turmas,
+    niveis
   );
 }
