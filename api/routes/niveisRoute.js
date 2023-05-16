b3844956a786f3c81bd62ed339545e45132e2513
Samuel Nascimento <sfsnascimento@gmail.com>
2023-05-16 16:31:59 -0300

    criando rota POST /niveis

--- api/routes/niveisRoute.js
+++ api/routes/niveisRoute.js
@@ -5,6 +5,7 @@ const router = Router();
 
 router
   .get('/niveis', NivelController.pegaTodosOsNiveis)
-  .get('/niveis/:id', NivelController.pegaUmNivel);
+  .get('/niveis/:id', NivelController.pegaUmNivel)
+  .post('/niveis', NivelController.criaNivel);
 
 module.exports = router;
