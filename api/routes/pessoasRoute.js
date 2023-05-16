532dffc0ec4cef6f1f22438be54a04fc6eb9a977
Samuel Nascimento <sfsnascimento@gmail.com>
2023-05-16 11:38:36 -0300

    criando rota /pessoas/:id

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -3,6 +3,8 @@ const PessoaController = require('../controller/PessoaController');
 
 const router = Router();
 
-router.get('/pessoas', PessoaController.pegaTodasAsPessoas);
+router
+  .get('/pessoas', PessoaController.pegaTodasAsPessoas)
+  .get('/pessoas/:id', PessoaController.pegaUmaPessoa);
 
 module.exports = router;
