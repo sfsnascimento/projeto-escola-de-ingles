2428c2602b5f5fd9fd7652de01a7529afaa407f4
Samuel Nascimento <sfsnascimento@gmail.com>
2023-05-16 15:59:55 -0300

    criando método pegaUmaTurma

--- api/controller/TurmaController.js
+++ api/controller/TurmaController.js
@@ -9,6 +9,16 @@ class TurmaController {
       return res.status(404).json(error.message);
     }
   }
+
+  static async pegaUmaTurma(req, res) {
+    const { id } = req.params;
+    try {
+      const pegaUmaTurma = await Turmas.findOne({ where: { id: +id } });
+      return res.status(200).json(pegaUmaTurma);
+    } catch (error) {
+      return res.status(404).json(error.message);
+    }
+  }
 }
 
 module.exports = TurmaController;
