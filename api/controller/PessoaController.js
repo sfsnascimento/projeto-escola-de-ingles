1c7b3cb6a147cc83250a02476ec401129aa0cb58
Samuel Nascimento <sfsnascimento@gmail.com>
2023-05-16 11:52:42 -0300

    desestruturando importação

--- api/controller/PessoaController.js
+++ api/controller/PessoaController.js
@@ -1,9 +1,9 @@
-const database = require('../db/models');
+const { Pessoas } = require('../db/models');
 
 class PessoaController {
   static async pegaTodasAsPessoas(_req, res) {
     try {
-      const pegaTodasAsPessoas = await database.Pessoas.findAll();
+      const pegaTodasAsPessoas = await Pessoas.findAll();
       return res.status(200).json(pegaTodasAsPessoas);
     } catch (error) {
       return res.status(500).json(error.message);
@@ -13,7 +13,7 @@ class PessoaController {
   static async pegaUmaPessoa(req, res) {
     const { id } = req.params;
     try {
-      const pegaUmaPessoa = await database.Pessoas.findOne({ where: +id });
+      const pegaUmaPessoa = await Pessoas.findOne({ where: +id });
       return res.status(200).json(pegaUmaPessoa);
     } catch (error) {
       return res.status(500).json(error.message);
@@ -23,7 +23,7 @@ class PessoaController {
   static async criaPessoa(req, res) {
     const novaPessoa = req.body;
     try {
-      const novaPessoaCriada = await database.Pessoas.create(novaPessoa);
+      const novaPessoaCriada = await Pessoas.create(novaPessoa);
       return res.status(200).json(novaPessoaCriada);
     } catch (error) {
       return res.status(500).json(error.message);
