1582ac89c352a0519623edc83439d6c38be212c7
Samuel Nascimento <sfsnascimento@gmail.com>
2023-05-16 16:29:06 -0300

    criando rota GET /niveis/:id

--- api/routes/niveisRoute.js
+++ api/routes/niveisRoute.js
@@ -4,6 +4,7 @@ const NivelController = require('../controller/NivelController');
 const router = Router();
 
 router
-  .get('/niveis', NivelController.pegaTodosOsNiveis);
+  .get('/niveis', NivelController.pegaTodosOsNiveis)
+  .get('/niveis/:id', NivelController.pegaUmNivel);
 
 module.exports = router;
