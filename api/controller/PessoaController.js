af940a10e48f3ca7be68ac4e6a0c64ed0956a258
Samuel Nascimento <sfsnascimento@gmail.com>
2023-05-16 11:38:11 -0300

    criando método pega uma pessoa

--- api/controller/PessoaController.js
+++ api/controller/PessoaController.js
@@ -1,10 +1,20 @@
 const database = require('../db/models');
 
 class PessoaController {
-  static async pegaTodasAsPessoas(req, res) {
+  static async pegaTodasAsPessoas(_req, res) {
     try {
       const pegaTodasAsPessoas = await database.Pessoas.findAll();
       return res.status(200).json(pegaTodasAsPessoas);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async pegaUmaPessoa(req, res) {
+    const { id } = req.params;
+    try {
+      const pegaUmaPessoa = await database.Pessoas.findOne({ where: +id });
+      return res.status(200).json(pegaUmaPessoa);
     } catch (error) {
       return res.status(500).json(error.message);
     }
