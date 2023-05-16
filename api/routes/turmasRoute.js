9311c23aa58940086a2652a5400c9bf1b96da099
Samuel Nascimento <sfsnascimento@gmail.com>
2023-05-16 16:00:16 -0300

    criando rota /turmas/:id

--- api/routes/turmasRoute.js
+++ api/routes/turmasRoute.js
@@ -4,6 +4,7 @@ const TurmaController = require('../controller/TurmaController');
 const router = Router();
 
 router
-  .get('/turmas', TurmaController.pegaTodasAsTurmas);
+  .get('/turmas', TurmaController.pegaTodasAsTurmas)
+  .get('/turmas/:id', TurmaController.pegaUmaTurma);
 
 module.exports = router;
