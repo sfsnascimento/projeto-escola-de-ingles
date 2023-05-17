f917edb872c5516d0debdb9c712aae8637ee9603
Samuel Nascimento <sfsnascimento@gmail.com>
2023-05-16 21:56:10 -0300

    criando rota POST para criar uma matricula

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -9,6 +9,7 @@ router
   .post('/pessoas', PessoaController.criaPessoa)
   .put('/pessoas/:id', PessoaController.atualizaPessoa)
   .delete('/pessoas/:id', PessoaController.apagaPessoa)
-  .get('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.pegaUmaMatricula);
+  .get('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.pegaUmaMatricula)
+  .post('/pessoas/:estudanteId/matricula', PessoaController.criaMatricula);
 
 module.exports = router;
