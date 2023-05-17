413c0906080ad17551d63bb68451fda162d0d39e
Samuel Nascimento <sfsnascimento@gmail.com>
2023-05-16 22:05:56 -0300

    criando rota PUT para atualizar uma matricula

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -10,6 +10,7 @@ router
   .put('/pessoas/:id', PessoaController.atualizaPessoa)
   .delete('/pessoas/:id', PessoaController.apagaPessoa)
   .get('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.pegaUmaMatricula)
-  .post('/pessoas/:estudanteId/matricula', PessoaController.criaMatricula);
+  .post('/pessoas/:estudanteId/matricula', PessoaController.criaMatricula)
+  .put('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.atualizaMatricula);
 
 module.exports = router;
