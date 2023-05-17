f02704c1d6955d8c7ada0dea20967b1e42c2d8d1
Samuel Nascimento <sfsnascimento@gmail.com>
2023-05-16 22:15:32 -0300

    criando rota DELETE para deletar uma matricula

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -11,6 +11,7 @@ router
   .delete('/pessoas/:id', PessoaController.apagaPessoa)
   .get('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.pegaUmaMatricula)
   .post('/pessoas/:estudanteId/matricula', PessoaController.criaMatricula)
-  .put('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.atualizaMatricula);
+  .put('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.atualizaMatricula)
+  .delete('/matricula/:matriculaId', PessoaController.apagaMatricula);
 
 module.exports = router;
